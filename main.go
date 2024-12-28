eff87151266a65e9a6ba2afeab865fb2e27076b8
0ya-sh0 <ymthatte.gmail.com>
2024-12-28 12:17:44 +0530

    KF-05: logging setup

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/0ya-sh0/keyforge/cmd"
+import (
+	"github.com/0ya-sh0/keyforge/cmd"
+	"github.com/0ya-sh0/keyforge/internal/logger"
+)
 
 func main() {
+	defer logger.Sync()
+	logger.Info("Starting KeyForge")
 	cmd.Execute()
 }
